4754305304419da9685e80e9ae7963cd98be6b66
iAHTOH <k.ahtoh@gmail.com>
2022-03-13 09:38:08 +0300

    Update entrypoint.sh
    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ set -x
 
 sed -e "s#{OR_PORT}#$OR_PORT#" \
     -e "s#{PT_PORT}#$PT_PORT#" \
-    -e "s#{OBFS4_ADR}#$OBFS4_ADR#" \
-#    -e "s#{CONTACT_INFO}#$CONTACT_INFO#" \
+#    -e "s#{OBFS4_ADR}#$OBFS4_ADR#" \
+    -e "s#{CONTACT_INFO}#$CONTACT_INFO#" \
     /torrc.template >/tmp/torrc
 
 exec "$@"
