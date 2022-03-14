2e0300846cc5edcb3eec8ed9ab99fe0e5a562351
iAHTOH <k.ahtoh@gmail.com>
2022-03-14 15:32:35 +0300

    Update entrypoint.sh
    Удалил логин с паролем

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,8 +9,6 @@ set -x
 
 sed -e "s#{LISTING_PORT}#$LISTING_PORT#" \
     -e "s#{OBFS4_ADR}#$OBFS4_ADR#" \
-    -e "s#{TOR_LOGIN}#$TOR_LOGIN#" \
-    -e "s#{TOR_PASS}#$TOR_PASS#" \
     -e "s#{CONTACT_INFO}#$CONTACT_INFO#" \
     /torrc.template >/tmp/torrc
 
