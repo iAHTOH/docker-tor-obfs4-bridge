47113925d8bcd318ba3aa683b576694c4470e212
iAHTOH <k.ahtoh@gmail.com>
2022-03-14 12:41:15 +0300

    Update entrypoint.sh
    Новые переменные

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,15 @@
 
 set -e
 
-[ -z "$OR_PORT" ] && echo '$OR_PORT undefined' && exit 1
-[ -z "$PT_PORT" ] && echo '$PT_PORT undefined' && exit 1
+[ -z "$LISTING_PORT" ] && echo '$LISTING_PORT undefined' && exit 1
 
 
 set -x
 
-sed -e "s#{OR_PORT}#$OR_PORT#" \
-    -e "s#{PT_PORT}#$PT_PORT#" \
+sed -e "s#{LISTING_PORT}#$LISTING_PORT#" \
+    -e "s#{OBFS4_ADR}#$OBFS4_ADR#" \
+    -e "s#{TOR_LOGIN}#$TOR_LOGIN#" \
+    -e "s#{TOR_PASS}#$TOR_PASS#" \
     -e "s#{CONTACT_INFO}#$CONTACT_INFO#" \
     /torrc.template >/tmp/torrc
 
