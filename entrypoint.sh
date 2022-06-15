8b5e01c83a3962ca0cb944959de2a1cb0a3a762e
iAHTOH <k.ahtoh@gmail.com>
2022-06-15 11:43:21 +0300

    4 addr for obfs4

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,10 @@ set -e
 set -x
 
 sed  -e "s#{LISTING_PORT}#$LISTING_PORT#" \
-     -e "s#{OBFS4_ADR}#$OBFS4_ADR#" \
+     -e "s#{OBFS4_ADR1}#$OBFS4_ADR1#" \
+     -e "s#{OBFS4_ADR2}#$OBFS4_ADR2#" \
+     -e "s#{OBFS4_ADR3}#$OBFS4_ADR3#" \
+     -e "s#{OBFS4_ADR4}#$OBFS4_ADR4#" \
      /torrc.template >/tmp/torrc
 
 exec "$@"
