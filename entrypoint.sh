6392dba051be813afdb6d689b7d677b2680ce2d8
AHTOH <k.ahtoh@gmail.com>
2023-12-10 09:52:33 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ set -e
 set -x
 
 sed  -e "s#{LISTING_PORT}#$LISTING_PORT#" \
+     -e "s#{OR_PORT}#$OR_PORT#" \
      -e "s#{OBFS4_ADR1}#$OBFS4_ADR1#" \
      -e "s#{OBFS4_ADR2}#$OBFS4_ADR2#" \
      -e "s#{OBFS4_ADR3}#$OBFS4_ADR3#" \
